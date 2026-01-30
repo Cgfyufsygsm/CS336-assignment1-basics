e7397425cf815cc650d6b6105008ce3de3715fdd
Taoyu Yang <i@imyangty.com>
2026-01-30 23:25:40 +0800

    ablation: remove rmsnorm

--- cs336_basics/nn/layers.py
+++ cs336_basics/nn/layers.py
@@ -37,19 +37,6 @@ class Embedding(nn.Module):
     def forward(self, x):
         return self.weight[x]
 
-class RMSNorm(nn.Module):
-    def __init__(self, d_model: int, eps: float = 1e-5, device=None, dtype=None):
-        super().__init__()
-        self.d_model = d_model
-        self.eps = eps
-        self.gain = nn.Parameter(torch.ones(d_model, device=device, dtype=dtype))
-
-    def forward(self, x):
-        in_dtype = x.dtype
-        x = x.to(torch.float32)
-        rms = torch.sqrt(torch.mean(x * x, dim=-1, keepdim=True) + self.eps)
-        return ((x / rms) * self.gain).to(in_dtype)
-
 class RotaryPositionalEmbedding(nn.Module):
     def __init__(self, theta: float, d_k: int, max_seq_len: int, device=None):
         super().__init__()
--- cs336_basics/nn/transformer.py
+++ cs336_basics/nn/transformer.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 from cs336_basics.nn.attention import MultiheadSelfAttention
-from cs336_basics.nn.layers import Embedding, Linear, RMSNorm
+from cs336_basics.nn.layers import Embedding, Linear
 from cs336_basics.nn.layers import RotaryPositionalEmbedding
 from cs336_basics.nn.network import SwiGLU
 from cs336_basics.nn.decoding import generate_tokens
@@ -16,15 +16,13 @@ class TransformerBlock(nn.Module):
         d_ff: int Dimensionality of the position-wise feed-forward inner layer.
         """
         super().__init__()
-        self.norm1 = RMSNorm(d_model)
         rope = RotaryPositionalEmbedding(theta, d_model // num_heads, max_seq_len=max_seq_len)
         self.attn = MultiheadSelfAttention(d_model, num_heads, rope)
-        self.norm2 = RMSNorm(d_model)
         self.ffn = SwiGLU(d_model, d_ff)
 
     def forward(self, x: torch.Tensor):
-        y = x + self.attn(self.norm1(x))
-        return y + self.ffn(self.norm2(y))
+        y = x + self.attn(x)
+        return y + self.ffn(y)
     
 class TransformerLM(nn.Module):
     def __init__(self, d_model: int, num_heads: int, d_ff: int, vocab_size: int, context_length: int, num_layers: int, rope_theta: float = 100000.0):
@@ -37,13 +35,11 @@ class TransformerLM(nn.Module):
             theta=rope_theta,
             max_seq_len=context_length
         ) for _ in range(num_layers)])
-        self.norm = RMSNorm(d_model)
         self.linear = Linear(d_model, vocab_size)
         
     def forward(self, x: torch.Tensor):
         x = self.token_embedding(x)
         x = self.attention_blocks(x)
-        x = self.norm(x)
         x = self.linear(x)
         return x
 
